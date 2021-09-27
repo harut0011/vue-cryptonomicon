b5e642353cacdccf110589082af54d5ffc88d57d
Harut <metsatunyanharut7@gmail.com>
2021-09-27 16:24:55 +0300

    computed and watch

--- src/App.vue
+++ src/App.vue
@@ -40,30 +40,93 @@
                                 type="text"
                                 name="wallet"
                                 id="wallet"
-                                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                                class="
+                                    block
+                                    w-full
+                                    pr-10
+                                    border-gray-300
+                                    text-gray-900
+                                    focus:outline-none
+                                    focus:ring-gray-500
+                                    focus:border-gray-500
+                                    sm:text-sm
+                                    rounded-md
+                                "
                                 placeholder="Например DOGE"
                             />
                         </div>
                         <div
-                            class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+                            class="
+                                flex
+                                bg-white
+                                shadow-md
+                                p-1
+                                rounded-md
+                                shadow-md
+                                flex-wrap
+                            "
                         >
                             <span
-                                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                                class="
+                                    inline-flex
+                                    items-center
+                                    px-2
+                                    m-1
+                                    rounded-md
+                                    text-xs
+                                    font-medium
+                                    bg-gray-300
+                                    text-gray-800
+                                    cursor-pointer
+                                "
                             >
                                 BTC
                             </span>
                             <span
-                                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                                class="
+                                    inline-flex
+                                    items-center
+                                    px-2
+                                    m-1
+                                    rounded-md
+                                    text-xs
+                                    font-medium
+                                    bg-gray-300
+                                    text-gray-800
+                                    cursor-pointer
+                                "
                             >
                                 DOGE
                             </span>
                             <span
-                                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                                class="
+                                    inline-flex
+                                    items-center
+                                    px-2
+                                    m-1
+                                    rounded-md
+                                    text-xs
+                                    font-medium
+                                    bg-gray-300
+                                    text-gray-800
+                                    cursor-pointer
+                                "
                             >
                                 BCH
                             </span>
                             <span
-                                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                                class="
+                                    inline-flex
+                                    items-center
+                                    px-2
+                                    m-1
+                                    rounded-md
+                                    text-xs
+                                    font-medium
+                                    bg-gray-300
+                                    text-gray-800
+                                    cursor-pointer
+                                "
                             >
                                 CHD
                             </span>
@@ -76,7 +139,28 @@
                 <button
                     @click="add"
                     type="button"
-                    class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                    class="
+                        my-4
+                        inline-flex
+                        items-center
+                        py-2
+                        px-4
+                        border border-transparent
+                        shadow-sm
+                        text-sm
+                        leading-4
+                        font-medium
+                        rounded-full
+                        text-white
+                        bg-gray-600
+                        hover:bg-gray-700
+                        transition-colors
+                        duration-300
+                        focus:outline-none
+                        focus:ring-2
+                        focus:ring-offset-2
+                        focus:ring-gray-500
+                    "
                 >
                     <!-- Heroicon name: solid/mail -->
                     <svg
@@ -98,14 +182,62 @@
             <template v-if="tickers.length > 0">
                 <div>
                     <button
-                        class="my-4 mx-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-                        @click="currentPage > 1 ? currentPage = currentPage - 1 : currentPage = currentPage"
+                        class="
+                            my-4
+                            mx-4
+                            inline-flex
+                            items-center
+                            py-2
+                            px-4
+                            border border-transparent
+                            shadow-sm
+                            text-sm
+                            leading-4
+                            font-medium
+                            rounded-full
+                            text-white
+                            bg-gray-600
+                            hover:bg-gray-700
+                            transition-colors
+                            duration-300
+                            focus:outline-none
+                            focus:ring-2
+                            focus:ring-offset-2
+                            focus:ring-gray-500
+                        "
+                        @click="
+                            currentPage > 1
+                                ? (currentPage = currentPage - 1)
+                                : (currentPage = currentPage)
+                        "
                         v-if="currentPage >= 2"
                     >
                         Назад
                     </button>
                     <button
-                        class="my-4 mx-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                        class="
+                            my-4
+                            mx-4
+                            inline-flex
+                            items-center
+                            py-2
+                            px-4
+                            border border-transparent
+                            shadow-sm
+                            text-sm
+                            leading-4
+                            font-medium
+                            rounded-full
+                            text-white
+                            bg-gray-600
+                            hover:bg-gray-700
+                            transition-colors
+                            duration-300
+                            focus:outline-none
+                            focus:ring-2
+                            focus:ring-offset-2
+                            focus:ring-gray-500
+                        "
                         @click="currentPage = currentPage + 1"
                         v-if="hasNextPage"
                     >
@@ -119,22 +251,39 @@
                 <hr class="w-full border-t border-gray-600 my-4" />
                 <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
                     <div
-                        v-for="t in filteredTickers()"
+                        v-for="t in paginatedTickers"
                         :key="t.name"
                         @click="select(t)"
                         :class="{
-                            'border-4': sel === t,
+                            'border-4': selectedTicker === t,
                         }"
-                        class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+                        class="
+                            bg-white
+                            overflow-hidden
+                            shadow
+                            rounded-lg
+                            border-purple-800 border-solid
+                            cursor-pointer
+                        "
                     >
                         <div class="px-4 py-5 sm:p-6 text-center">
                             <dt
-                                class="text-sm font-medium text-gray-500 truncate"
+                                class="
+                                    text-sm
+                                    font-medium
+                                    text-gray-500
+                                    truncate
+                                "
                             >
                                 {{ t.name }} - USD
                             </dt>
                             <dd
-                                class="mt-1 text-3xl font-semibold text-gray-900"
+                                class="
+                                    mt-1
+                                    text-3xl
+                                    font-semibold
+                                    text-gray-900
+                                "
                             >
                                 {{ t.price }}
                             </dd>
@@ -142,7 +291,23 @@
                         <div class="w-full border-t border-gray-200"></div>
                         <button
                             @click.stop="handleDelete(t)"
-                            class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+                            class="
+                                flex
+                                items-center
+                                justify-center
+                                font-medium
+                                w-full
+                                bg-gray-100
+                                px-4
+                                py-4
+                                sm:px-6
+                                text-md text-gray-500
+                                hover:text-gray-600
+                                hover:bg-gray-200
+                                hover:opacity-20
+                                transition-all
+                                focus:outline-none
+                            "
                         >
                             <svg
                                 class="h-5 w-5"
@@ -162,22 +327,27 @@
                 </dl>
                 <hr class="w-full border-t border-gray-600 my-4" />
             </template>
-            <section v-if="sel" class="relative">
+            <section v-if="selectedTicker" class="relative">
                 <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-                    {{ sel.name }} - USD
+                    {{ selectedTicker.name }} - USD
                 </h3>
                 <div
-                    class="flex items-end border-gray-600 border-b border-l h-64"
+                    class="
+                        flex
+                        items-end
+                        border-gray-600 border-b border-l
+                        h-64
+                    "
                 >
                     <div
-                        v-for="(bar, idx) in normalizeGraph()"
+                        v-for="(bar, idx) in normalizedGraph"
                         :key="idx"
                         :style="{ height: `${bar}%` }"
                         class="bg-purple-800 border w-10"
                     ></div>
                 </div>
                 <button
-                    @click="sel = null"
+                    @click="selectedTicker = null"
                     type="button"
                     class="absolute top-0 right-0"
                 >
@@ -191,7 +361,7 @@
                         x="0"
                         y="0"
                         viewBox="0 0 511.76 511.76"
-                        style="enable-background:new 0 0 512 512"
+                        style="enable-background: new 0 0 512 512"
                         xml:space="preserve"
                     >
                         <g>
@@ -213,18 +383,18 @@ export default {
     name: 'App',
     data() {
         return {
-            tickerList: null,
-            ticker: null,
+            ticker: '',
             tickers: [],
-            sel: null,
+            selectedTicker: null,
             graph: [],
             currentPage: 1,
             pageFilter: '',
-            hasNextPage: true,
         };
     },
     created() {
-        const windowData = Object.fromEntries(new URL(window.location).searchParams.entries());
+        const windowData = Object.fromEntries(
+            new URL(window.location).searchParams.entries()
+        );
 
         if (windowData.filter) {
             this.pageFilter = windowData.filter;
@@ -243,17 +413,43 @@ export default {
             });
         }
     },
-    methods: {
-        filteredTickers() {
-            const start = (this.currentPage - 1) * 6;
-            const end = this.currentPage * 6;
-
-            const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.pageFilter));
-
-            this.hasNextPage = filteredTickers.length > end;
-
-            return filteredTickers.slice(start, end);
+    computed: {
+        startIndex() {
+            return (this.currentPage - 1) * 6
         },
+        endIndex() {
+            return this.currentPage * 6
+        },
+        filteredTickers() {
+            return this.tickers.filter(ticker => ticker.name.includes(this.pageFilter));
+        },
+        paginatedTickers() {
+            return this.filteredTickers.slice(this.startIndex, this.endIndex);
+        },
+        hasNextPage() {
+            return this.filteredTickers.length > this.endIndex;
+        },
+        normalizedGraph() {
+            const maxVal = Math.max(...this.graph);
+            const minVal = Math.min(...this.graph);
+
+            if (maxVal === minVal) {
+                return this.graph.map(() => 50)
+            }
+
+            return this.graph.map(
+                (price) => 5 + ((price - minVal) * 95) / (maxVal - minVal)
+            );
+        },
+
+        pageStateOptions() {
+            return {
+                filter: this.pageFilter,
+                page: this.currentPage
+            }
+        }
+    },
+    methods: {
         subscribeToUpdates(tickerName) {
             setInterval(async () => {
                 if (tickerName) {
@@ -268,7 +464,7 @@ export default {
                             ? data.USD.toFixed(2)
                             : data.USD.toPrecision(2);
 
-                    if (this.sel?.name === tickerName) {
+                    if (this.selectedTicker?.name === tickerName) {
                         this.graph.push(data.USD);
                     }
                 }
@@ -280,9 +476,7 @@ export default {
                 price: '-',
             };
 
-            this.tickers.push(currentTicker);
-
-            localStorage.setItem('crypto-list', JSON.stringify(this.tickers));
+            this.tickers = [...this.tickers, currentTicker];
 
             this.subscribeToUpdates(currentTicker.name);
 
@@ -291,39 +485,39 @@ export default {
         },
         handleDelete(tickerToRemove) {
             this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+            if (this.selectedTicker) {
+                this.selectedTicker = null
+            }
         },
         select(ticker) {
-            this.sel = ticker;
+            this.selectedTicker = ticker;
             this.graph = [];
-        },
-
-        normalizeGraph() {
-            const maxVal = Math.max(...this.graph);
-            const minVal = Math.min(...this.graph);
-
-            return this.graph.map(
-                (price) => 5 + ((price - minVal) * 95) / (maxVal - minVal)
-            );
         },
     },
     watch: {
+        selectedTicker() {
+            this.graph = [];
+        },
+        tickers() {
+            localStorage.setItem('crypto-list', JSON.stringify(this.tickers));
+        },
+        paginatedTickers() {
+            if (this.paginatedTickers.length === 0 && this.currentPage > 1) {
+                this.currentPage -= 1;
+            }
+        },
+
         pageFilter() {
             this.pageFilter = this.pageFilter.toUpperCase();
             this.currentPage = 1;
-
+        },
+        pageStateOptions(v) {
             window.history.pushState(
-                null, 
-                document.title, 
-                `${window.location.pathname}?filter=${this.pageFilter}&page=${this.currentPage}`
+                null,
+                document.title,
+                `${window.location.pathname}?filter=${v.filter}&page=${v.page}`
             );
         },
-        currentPage() {
-            window.history.pushState(
-                null, 
-                document.title, 
-                `${window.location.pathname}?filter=${this.pageFilter}&page=${this.currentPage}`
-            );
-        }
-    }
+    },
 };
 </script>
